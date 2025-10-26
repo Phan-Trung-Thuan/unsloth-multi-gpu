c6b8b3563af7395037a56a5da13b6de91b9f8924
Phan Trung Thuận <thuantk000@gmail.com>
2025-10-26 18:47:26 +0700

    update

--- train-ddp.py
+++ train-ddp.py
@@ -43,7 +43,7 @@ MODEL_PATH     = "Qwen/Qwen3-1.7B"
 MAX_LEN        = 1024
 LR             = 2e-5
 SAVE_STEPS     = 2000
-TARGET_GLOBAL_BATCH = 24
+TARGET_GLOBAL_BATCH = 512
 RESUME_FROM    = None  # e.g., "checkpoints/checkpoint-1000"
 
 DATASET_PATH   = "musicpile_cluster_filtered"  # your saved dataset folder
