f91c58040839614e75fe7e013cf6b52abdc0de49
Phan Trung Thuận <thuantk000@gmail.com>
2025-11-01 12:56:46 +0700

    update

--- prepare-rl-dataset.py
+++ prepare-rl-dataset.py
@@ -64,7 +64,7 @@ def main():
     print('Finished sampling_params object')
 
     new_rejected = []
-    batch_size = 64
+    batch_size = 256
     num_rows = len(ds)
 
     for start in tqdm(range(0, len(ds), batch_size)):
