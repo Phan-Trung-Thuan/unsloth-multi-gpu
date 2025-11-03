885cc3ca903e7467968022e9ae5ff863b761b1cf
Phan Trung Thuận <thuantk000@gmail.com>
2025-11-03 10:04:48 +0700

    update

--- train-ddp.py
+++ train-ddp.py
@@ -38,13 +38,13 @@ log = logging.getLogger("train_sft")
 log.info(f"RANK={RANK} LOCAL_RANK={LOCAL_RANK} WORLD_SIZE={WORLD_SIZE} device={device}")
 
 # --------------------------- User config -----------------------------
-LORA_RANK      = 16
+LORA_RANK      = 32
 NUM_EPOCHS     = 1
 MODEL_PATH     = "Qwen/Qwen3-8B"
 MAX_LEN        = 1024
 LR             = 2e-4
 SAVE_STEPS     = 1000
-TARGET_GLOBAL_BATCH = 1
+TARGET_GLOBAL_BATCH = 1024
 RESUME_FROM    = None  # e.g., "checkpoints/checkpoint-1000"
 
 DATASET_PATH   = "musicpile_cluster_filtered_v2"  # your saved dataset folder
